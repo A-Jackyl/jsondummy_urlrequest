7b2d1460790afdbae7ec62dc38cb7f60000bebf7
A-Jackyl <thebroomhunter@gmail.com>
2024-03-07 23:23:41 +0100

    Fixed up styling

--- assets/js/card.js
+++ assets/js/card.js
@@ -36,7 +36,7 @@ function GetProductsByCategory(myCategoryURL) {
             return result.json()
         })
         .then((json) => {
-            recivedProductsByCategory(json)
+            receivedProductsByCategory(json)
         });
 }
 
@@ -292,11 +292,18 @@ function CreateProductView(CardJS) {
         let myHTML = `
         <figure class="product_card_item" href="https://google.com">
             <h2>${product.title}</h2>
+            <p class="card_item_cat">${product.category}</p>
             <img src="${product.images[1]}"></img>
             <p class="card_item_desc">${product.description}</p>
             <div class="card_bottom">
-                <p>Price: $${product.price}</p>
-                <a class="bottom_btn" href="#">Buy</a>
+                <p class="card_item_price">$ ${product.price}<span style="font-size: 2rem;">.00</span></p>
+                <svg class="bottom_btn" fill="#219EBC" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                viewBox="0 0 330 330" xml:space="preserve">
+           <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+               c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+               C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+               C255,161.018,253.42,157.202,250.606,154.389z"/>
+           </svg>
             </div>
         </figure>
         `
@@ -345,3 +352,4 @@ function buildProduct(product) {
     `
     productSection.innerHTML = myHTML
 }
+
